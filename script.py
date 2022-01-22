0f855ea206929f67d3a00a56466b4ef314b00e8c
Laura Goon <goon.laura@gmail.com>
2022-01-21 20:13:16 -0800

    finishes a whole game!

--- script.py
+++ script.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.service import Service
+import time
 
 DRIVER = None
 KEYBOARD_ROWS_TILES = list()
@@ -44,13 +45,13 @@ def map_word_scores(curr_words):
 
 # Get word with highest score with given conditions
 # TODO: more efficient on yellow chrs
-def next_word(word_scores, try_num, yellow_chrs=set()):
+def next_word(word_scores, feedback_regex, yellows):
     found_word = False
     ret_word = ""
 
     while not found_word:
         curr_word = max(word_scores.items(), key=lambda tup: tup[1])[0]
-        if bool(re.match("[a-z][a-z][a-z][a-z][a-z]", curr_word)) and len(set(list(curr_word)).intersection(set(yellow_chrs))) == len(yellow_chrs):
+        if bool(re.match(feedback_regex, curr_word)) and len(set(curr_word).intersection(yellows)) == len(yellows):
             ret_word = curr_word
             found_word = True
         else:
@@ -125,18 +126,19 @@ def connect_site():
     gen_site_globals()
 
     # close instruction pop-up
+    time.sleep(1)
     game_app = driver.find_element(By.TAG_NAME, "game-app")
     game_app.click()
 
 # Determine if game has been won
 def game_won(try_num):
     recent_board_row = BOARD_ROWS_TILES[try_num-1]
-    last_tile = recent_board_row[-1]
-    last_tile_div = DRIVER.execute_script('return arguments[0].shadowRoot.children', last_tile)[1]
     correct_so_far = True
 
     for i in range(5):
-        correct_so_far = correct_so_far and last_tile_div.get_attribute("data-state") == "correct"
+        curr_tile = recent_board_row[i]
+        curr_tile_div = DRIVER.execute_script('return arguments[0].shadowRoot.children', curr_tile)[1]
+        correct_so_far = correct_so_far and curr_tile_div.get_attribute("data-state") == "correct"
 
     return correct_so_far
 
@@ -148,6 +150,85 @@ def word_not_in_list(try_num):
 
     return last_tile_div.get_attribute("data-state") == "tbd"
 
+# Get feedback from previous try
+def get_feedback(try_num):
+    recent_board_row = BOARD_ROWS_TILES[try_num-1]
+    feedback_map = {"green":{}, "yellow":{}, "grey":set()}
+
+    for i in range(5):
+        curr_tile = recent_board_row[i]
+        curr_tile_div = DRIVER.execute_script('return arguments[0].shadowRoot.children', curr_tile)[1]
+        curr_tile_state = curr_tile_div.get_attribute("data-state")
+
+        if curr_tile_state == "correct":
+            feedback_map["green"][i] = curr_tile.get_attribute("letter")
+        elif curr_tile_state == "present":
+            feedback_map["yellow"][i] = curr_tile.get_attribute("letter")
+        elif curr_tile_state == "absent":
+            feedback_map["grey"].add(curr_tile.get_attribute("letter"))
+        elif curr_tile_state == "empty":
+            raise ReferenceError("Empty tile")
+        elif curr_tile_state == "tbd":
+            raise ReferenceError("TBD tile")
+        else:
+            raise ReferenceError("Encountered weird tile state")
+
+    return feedback_map
+
+# Generate the regex based on feedback of letters
+def gen_regex(greens, yellows, greys):
+    ret_regex = ["","","","",""]
+
+    # fill in correct letters
+    for itm in greens.items():
+        idx = itm[0]
+        letter = itm[1]
+        ret_regex[idx] = "(?:[" + letter + "])"
+    
+    # fill in rest
+    for i in range(5):
+
+        # not found correct letter for this idx
+        if len(ret_regex[i]) == 0:
+
+            dont_include = set()
+            dont_include.update(greys)
+
+            if i in yellows.keys():
+                letters = yellows[i]
+                dont_include.update(letters)
+
+            if len(dont_include) > 0:
+                dont_include_letters = "".join(dont_include)
+                ret_regex[i] = "(?:(?![" + dont_include_letters + "])[a-z])"
+            else:
+                ret_regex[i] = "(?:[a-z])"
+
+    return "".join(ret_regex)
+
+# Update ds for greens/corrects
+def update_greens(greens, green_feedback):
+    for itm in green_feedback.items():
+        idx = itm[0]
+        letter = itm[1]
+
+        if idx not in greens.keys():
+            greens[idx] = letter
+
+    return greens
+
+# Update ds for yellows/presents
+def update_yellows(yellows, yellow_feedback):
+    for itm in yellow_feedback.items():
+        idx = itm[0]
+        letter = itm[1]
+
+        if idx not in yellows.keys():
+            yellows[idx] = set()
+        yellows[idx].add(letter)
+
+    return yellows
+
 # Run helper functions in order
 def run_script():
 
@@ -155,14 +236,23 @@ def run_script():
     curr_words = gen_words()
     word_scores = map_word_scores(curr_words)
     connect_site()
+
+    time.sleep(0.5) # delay so don't start typing before site loads
     
     # game
     has_game_finished = False
     try_num = 1
+    greens = {} # int : str
+    yellows = {} # int : set(str)
+    greys = set()
 
     while not has_game_finished: # 6 tries
-        now_word = next_word(word_scores, try_num)
+        feedback_regex = gen_regex(greens, yellows, greys)
+        yellow_chrs = yellows.values()
+        now_word = next_word(word_scores, feedback_regex, set().union(*yellow_chrs))
         type_word(now_word)
+
+        time.sleep(2)
 
         # won the game
         if game_won(try_num):
@@ -170,11 +260,15 @@ def run_script():
         
         # need to continue the game
         else:
-            if not word_not_in_list(try_num):
-                try_num += 1
-            else:
+            if word_not_in_list(try_num):
                 for i in range(5): # delete last word
                     type_keyboard("BACKSPACE")
+            else:
+                feedback = get_feedback(try_num)
+                greens = update_greens(greens, feedback["green"])
+                yellows = update_yellows(yellows, feedback["yellow"])
+                greys.update(feedback["grey"])
+                try_num += 1
 
             if try_num == 7: # finished try 6
                 has_game_finished = True
